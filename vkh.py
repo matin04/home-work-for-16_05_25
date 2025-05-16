21dd64eb0a4c05deeb624cdf0c24e7ee29d0efc9
matin04 <fathuddinzodamatin@gmail.com>
2025-05-16 12:37:44 +0300

    7

--- vkh.py
+++ vkh.py
@@ -1,3 +1,10 @@
-str1=input('')
-str2=str1.replace('е','и')
+str1=input()
+str2=0
+str3=0
+for i in str1:
+    if i.isupper():
+        str2+=1
+    elif i.islower():
+        str3+=1
 print(str2)
+print(str3)
